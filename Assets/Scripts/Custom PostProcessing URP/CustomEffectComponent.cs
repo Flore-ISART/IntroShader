9616fdbc798435b4aecc1ff62e9eb517bf058b02
floremeyerprevost@gmail.com <>
2023-02-20 00:52:35 +0100

    Add base shaders

---
+++ Assets/Scripts/Custom PostProcessing URP/CustomEffectComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[Serializable, VolumeComponentMenuForRenderPipeline("Custom/CustomEffectComponent", typeof(UniversalRenderPipeline))]
+public class CustomEffectComponent : VolumeComponent, IPostProcessComponent
+{
+    public MaterialParameter materials = new(default);
+
+    public bool IsActive() => active;
+   
+    public bool IsTileCompatible() => true;
+}
